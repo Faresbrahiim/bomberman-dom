6559d3b6f8dfaf516c5fc50f8e796f31c2018cb7
redd255 <youssef16hay@gmail.com>
2025-09-01 20:33:54 +0100

    css

--- client/main.js
+++
@@ -1,5 +0,0 @@
-import { Main } from './core/Main.js';
-
-const app = document.getElementById('app');
-// create instance of the ui exist in the -> main core (when make instance it will render ui direcly )
-new Main(app);
